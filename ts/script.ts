c6ee66a534585410c3eadd4af1941628ab2f9e59
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-12 10:12:19 -0300

    Final da aula 2

--- ts/script.ts
+++ ts/script.ts
@@ -1,9 +1,31 @@
-const containerVideos = document.querySelector('.videos__container');
+export interface Video {
+    "id": number,
+    "titulo": string,
+    "descricao": string,
+    "url": string,
+    "imagem": string,
+    "categoria": string
+}
+
+const containerVideos: HTMLElement = document.querySelector('.videos__container');
 
 const url = 'http://127.0.0.1:3000/videos';
 
-const api = fetch(url).then(res => res.json()).then(videos => {
-    videos.forEach(video => {
-
-    });
-})
+const api = fetch(url)
+    .then(res => res.json())
+    .then(videos => videos.forEach((video: Video) => {
+        containerVideos.innerHTML += `
+        <li class="videos__item">
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="1" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>
+            </div>
+        </li>
+        `;
+    })
+    )
+    .catch((error) => {
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
+    })

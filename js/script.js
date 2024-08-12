f62d059afffb4c8c8c695d3938264c9b9203871b
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-12 11:12:50 -0300

    Meio da aula 3

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
 const containerVideos = document.querySelector('.videos__container');
 const url = 'http://127.0.0.1:3000/videos';
-const api = fetch(url)
-    .then(res => res.json())
-    .then(videos => videos.forEach((video) => {
-    containerVideos.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="1" allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
-                <h3 class="titulo-video">${video.titulo}</h3>
-                <p class="titulo-canal">${video.descricao}</p>
-            </div>
-        </li>
-        `;
-}))
-    .catch((error) => {
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
-});
+async function carregarVideos() {
+    try {
+        const busca = await fetch(url);
+        const videos = await busca.json();
+        videos.forEach((video) => {
+            if (video.categoria == '') {
+                throw new Error(`Vídeo '${video.titulo}' não tem categoria!`);
+            }
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${video.url}" title="${video.titulo}" frameborder="1" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                    <h3 class="titulo-video">${video.titulo}</h3>
+                    <p class="titulo-canal">${video.descricao}</p>
+                </div>
+            </li>
+            `;
+        });
+    }
+    catch (error) {
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
+    }
+    finally {
+        console.log('Fim da requisiçcão de carregamento.');
+    }
+}
+carregarVideos();
 export {};
--- ts/script.ts
+++ ts/script.ts
@@ -11,21 +11,31 @@ const containerVideos: HTMLElement = document.querySelector('.videos__container'
 
 const url = 'http://127.0.0.1:3000/videos';
 
-const api = fetch(url)
-    .then(res => res.json())
-    .then(videos => videos.forEach((video: Video) => {
-        containerVideos.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="1" allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
-                <h3 class="titulo-video">${video.titulo}</h3>
-                <p class="titulo-canal">${video.descricao}</p>
-            </div>
-        </li>
-        `;
-    })
-    )
-    .catch((error) => {
+async function carregarVideos() {
+    try {
+        const busca = await fetch(url);
+        const videos = await busca.json();
+
+        videos.forEach((video: Video) => {
+            if (video.categoria == '') {
+                throw new Error(`Vídeo '${video.titulo}' não tem categoria!`);
+            }
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${video.url}" title="${video.titulo}" frameborder="1" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                    <h3 class="titulo-video">${video.titulo}</h3>
+                    <p class="titulo-canal">${video.descricao}</p>
+                </div>
+            </li>
+            `;
+        })
+    } catch (error) {
         containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
-    })
+    } finally {
+        console.log('Fim da requisiçcão de carregamento.');
+    }
+}
+
+carregarVideos();

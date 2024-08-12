f377f34d29ca7810de0c0c9a95dd7d8a03dbb88f
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-12 13:35:28 -0300

    Final do curso

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-const containerVideos = document.querySelector('.videos__container');
+// Definição do endereço do servidor
 const url = 'http://127.0.0.1:3000/videos';
+// EXTRA: criei essa variável para tentar novamente em 5 segundos se houver algum erro na conexão
+let nuncaDesista = null;
+// Função para carregar os vídeos do servidor
+const containerVideos = document.querySelector('.videos__container');
 async function carregarVideos() {
     try {
         const busca = await fetch(url);
         const videos = await busca.json();
+        containerVideos.innerHTML = '';
         videos.forEach((video) => {
             if (video.categoria == '') {
                 throw new Error(`Vídeo '${video.titulo}' não tem categoria!`);
@@ -15,17 +20,50 @@ async function carregarVideos() {
                     <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
             </li>
             `;
         });
+        // EXTRA: se tudo der certo depois de um erro, evita que a função seja chamada novamente
+        if (nuncaDesista) {
+            clearInterval(nuncaDesista);
+            nuncaDesista = null;
+        }
     }
     catch (error) {
         containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
-    }
-    finally {
-        console.log('Fim da requisiçcão de carregamento.');
+        // EXTRA: se houve um erro, chama a função novamente a cada 5 segundos
+        if (!nuncaDesista) {
+            nuncaDesista = setInterval(carregarVideos, 5000);
+        }
     }
 }
 carregarVideos();
+// Criação do filtro de pesquisa
+const barraDePesquisa = document.querySelector('.pesquisar__input');
+barraDePesquisa.oninput = () => {
+    const videos = document.querySelectorAll('.videos__item');
+    videos.forEach((video) => {
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+        const valorFiltro = barraDePesquisa.value.toLowerCase();
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+};
+// Criação do filtro de categoria
+const botaoCategoria = document.querySelectorAll('.superior__item');
+botaoCategoria.forEach((botao) => {
+    let filtroCategoria = botao.getAttribute('name');
+    botao.onclick = () => {
+        filtrarPorCategoria(filtroCategoria);
+    };
+});
+function filtrarPorCategoria(filtroCategoria) {
+    const valorFiltro = filtroCategoria.toLowerCase();
+    const videos = document.querySelectorAll('.videos__item');
+    videos.forEach((video) => {
+        const categoria = video.querySelector('.categoria').textContent.toLowerCase();
+        video.style.display = categoria.includes(valorFiltro) || valorFiltro == 'tudo' ? 'block' : 'none';
+    });
+}
 export {};
--- ts/script.ts
+++ ts/script.ts
@@ -1,3 +1,7 @@
+// Definição do endereço do servidor
+const url = 'http://127.0.0.1:3000/videos';
+
+// Modelo de cada dado gravado no arquivo json
 export interface Video {
     "id": number,
     "titulo": string,
@@ -7,14 +11,20 @@ export interface Video {
     "categoria": string
 }
 
-const containerVideos: HTMLElement = document.querySelector('.videos__container');
+// EXTRA: criei essa variável para tentar novamente em 5 segundos se houver algum erro na conexão
 
-const url = 'http://127.0.0.1:3000/videos';
+let nuncaDesista = null;
+
+// Função para carregar os vídeos do servidor
+
+const containerVideos: HTMLElement = document.querySelector('.videos__container');
 
 async function carregarVideos() {
     try {
         const busca = await fetch(url);
         const videos = await busca.json();
+
+        containerVideos.innerHTML = '';
 
         videos.forEach((video: Video) => {
             if (video.categoria == '') {
@@ -27,15 +37,56 @@ async function carregarVideos() {
                     <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
             </li>
             `;
         })
+        // EXTRA: se tudo der certo depois de um erro, evita que a função seja chamada novamente
+        if (nuncaDesista) {
+            clearInterval(nuncaDesista);
+            nuncaDesista = null;
+        }
     } catch (error) {
         containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
-    } finally {
-        console.log('Fim da requisiçcão de carregamento.');
+        // EXTRA: se houve um erro, chama a função novamente a cada 5 segundos
+        if (!nuncaDesista) {
+            nuncaDesista = setInterval(carregarVideos, 5000);
+        }
     }
 }
 
 carregarVideos();
+
+// Criação do filtro de pesquisa
+
+const barraDePesquisa: HTMLInputElement = document.querySelector('.pesquisar__input');
+
+barraDePesquisa.oninput = () => {
+    const videos = document.querySelectorAll('.videos__item');
+    videos.forEach((video: HTMLElement) => {
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+        const valorFiltro = barraDePesquisa.value.toLowerCase();
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+}
+
+// Criação do filtro de categoria
+
+const botaoCategoria = document.querySelectorAll('.superior__item');
+
+botaoCategoria.forEach((botao: HTMLElement) => {
+    let filtroCategoria = botao.getAttribute('name');
+    botao.onclick = () => {
+        filtrarPorCategoria(filtroCategoria);
+    }
+});
+
+function filtrarPorCategoria(filtroCategoria) {
+    const valorFiltro = filtroCategoria.toLowerCase();
+    const videos = document.querySelectorAll('.videos__item');
+    videos.forEach((video: HTMLElement) => {
+        const categoria = video.querySelector('.categoria').textContent.toLowerCase();
+        video.style.display = categoria.includes(valorFiltro) || valorFiltro == 'tudo' ? 'block' : 'none';
+    });
+}
